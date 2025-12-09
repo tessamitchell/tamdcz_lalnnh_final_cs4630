a9ac3e7f3d91f74c732023deeacebbce9e1312d9
tessamitchell <147358356+tessamitchell@users.noreply.github.com>
2025-12-09 15:13:41 -0600

    added unity files

---
+++ tamdcz_lalnnh_final/Assets/_Scripts/GridManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 mouseP = inputManager.GetSelectedMapPosition();
+        //Vector3Int gridP = gridWorldToCell(mouseP);
+        //mouseIndicator.transform.position = mouseP;
+        //cellIndicator
+
+    }
+}
